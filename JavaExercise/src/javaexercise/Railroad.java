a4f58984ffd8f6b5c048f487bc8df80b5374fe47
JZ <jingyu.zhan@outlook.com>
2018-11-15 15:11:38 +0100

    Don't quite understand the meaning of this question, but anyway...

---
+++ JavaExercise/src/javaexercise/Railroad.java
@@ -0,0 +1,22 @@
+package javaexercise;
+
+import java.util.Scanner;
+
+public class Railroad {
+
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if(y % 2 ==0) {
+			System.out.println("possible");
+		}else {
+			System.out.println("impossible");
+		}
+		
+		sc.close();
+	}
+
+}
