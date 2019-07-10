3400da5109b452d3bb8dca7df14c3964db88c548
Jingyu Zhan <jingyu.zhan@sigmatechnology.se>
2019-07-10 09:37:57 +0200

    soloved a real challenge. A simple Math problem..

---
+++ JavaExercise/src/javaexercise/ARealChallenge.java
@@ -0,0 +1,12 @@
+package javaexercise;
+
+import java.util.Scanner;
+
+public class ARealChallenge {
+
+	public static void main(String[] args) {
+		System.out.println(Math.sqrt(new Scanner(System.in).nextLong())*4);
+
+	}
+
+}
